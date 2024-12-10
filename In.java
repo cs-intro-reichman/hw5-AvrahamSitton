8ea1bbbefcc350565dbfe4fe8e1c6b6c9c32a030
AvrahamSitton <avraham.sitton@post.runi.ac.il>
2024-12-10 19:21:44 +0200

    letsPlay

--- In.java
+++ In.java
@@ -177,7 +177,7 @@ public final class In {
 
             // or URL from web
             if (url == null) {
-                url = new URL(name);
+                //url = new URL(name);
             }
 
             URLConnection site = url.openConnection();
